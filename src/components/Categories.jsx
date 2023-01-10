9a6f2c83957073e5135d23bb4626d59df3f13dd3
Morkovka171 <guganowa@yandex.ru>
2023-01-10 19:05:24 +0300

    test

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -3,7 +3,7 @@ function Categories() {
       <div class="categories">
         <ul>
           <li class="active">Все</li>
-          <li>Мясные</li>
+          <li>Мясные cegth</li>
           <li>Вегетарианская</li>
           <li>Гриль</li>
           <li>Острые</li>
